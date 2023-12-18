b41edc8d2a5a71b38c722d669455c945cfe1fa7a
stella-mumbi <stella.mumbi@student.moringaschool.com>
2023-12-18 08:57:36 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = function() {
+    return 2000 / 100;
+};
+
+// Arrow function called square that takes one parameter and multiplies it by itself
+const square = (num) => {
+    return num * num;
+};
+
+// Arrow function called add that takes two parameters and adds them together
+const add = (a, b) => {
+    return a + b;
+};
+
+// Example usage:
+console.log(divide()); // Output: 20
+console.log(square(5)); // Output: 25
+console.log(add(3, 7)); // Output: 10
